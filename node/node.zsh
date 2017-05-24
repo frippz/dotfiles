9602535ee19dd02120193d1d00038dec4902984d
Fredrik Frodlund <fredrik@frodlund.se>
2017-05-24 12:40:45 +0200

    Implement lazy loading of Node
    Fix #8 (maybe?)

--- node/node.zsh
+++ node/node.zsh
@@ -1,4 +1,4 @@
-function lazy_node() {
+function load_nodenv() {
   # Node environment config
   export NODE_PATH=$HOME/.npm/lib/node_modules
   export PATH=$PATH:$HOME/.npm/bin
@@ -8,4 +8,34 @@ function lazy_node() {
     export PATH="$HOME/.nodenv/bin:$PATH"
     eval "$(nodenv init -)"
   fi
+}
+
+node() {
+  unset -f node
+  load_nodenv
+  node "$@"
+}
+
+npm() {
+  unset -f npm
+  load_nodenv
+  npm "$@"
+}
+
+nodemon() {
+  unset -f nodemon
+  load_nodenv
+  nodemon "$@"
+}
+
+gulp() {
+  unset -f gulp
+  load_nodenv
+  gulp "$@"
+}
+
+yarn() {
+  unset -f yarn
+  load_nodenv
+  yarn "$@"
 }
