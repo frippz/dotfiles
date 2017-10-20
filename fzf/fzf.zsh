541d19e70a086fb4186be215ba019c3d524fa9c7
Fredrik Frodlund <fredrik@frodlund.se>
2017-10-20 09:08:31 +0200

    Add fzf config for ZSH

---
+++ fzf/fzf.zsh
@@ -0,0 +1,8 @@
+if [ -e /usr/local/opt/fzf/shell/completion.zsh ]; then
+  source /usr/local/opt/fzf/shell/key-bindings.zsh
+  source /usr/local/opt/fzf/shell/completion.zsh
+fi
+
+export FZF_DEFAULT_COMMAND='ag --nocolor -g ""'
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+export FZF_ALT_C_COMMAND="$FZF_DEFAULT_COMMAND"
