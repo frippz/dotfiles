0841258e93d2888e7acd7d6b4d4281d053f1eda8
Fredrik Frodlund <fredrik@frodlund.se>
2017-10-23 10:55:25 +0200

    Add node & yarn again and install global packages with them

--- node/install.sh
+++ node/install.sh
@@ -18,6 +18,19 @@ PACKAGES=(
   "svgo"
 )
 
+# Load nodenv if needed
+function load_nodenv() {
+  # Node environment config
+  export NODE_PATH=$HOME/.npm/lib/node_modules
+  export PATH=$PATH:$HOME/.npm/bin
+
+  # nodenv
+  if [ -d $HOME/.nodenv/bin ] ; then
+    export PATH="$HOME/.nodenv/bin:$PATH"
+    eval "$(nodenv init -)"
+  fi
+}
+
 set -e
 
 # Check for nodenv and install if it’s not installed
@@ -25,23 +38,33 @@ if [ ! -d $HOME/.nodenv/bin ] ; then
   echo ""
   echo "  √ nodenv not found. Installing now..."
   echo ""
+
   git clone https://github.com/nodenv/nodenv.git $HOME/.nodenv
+
   echo ""
   echo "  √ Reloading environment"
   echo ""
-  export PATH=$HOME/.nodenv/bin:$PATH
-  eval "$(nodenv init -)"
+
+  load_nodenv
+  # export PATH=$HOME/.nodenv/bin:$PATH
+  # eval "$(nodenv init -)"
+
   echo ""
   echo "  √ Installing node-build plugin..."
   echo ""
+
   git clone https://github.com/nodenv/node-build.git $HOME/.nodenv/plugins/node-build
+
   echo ""
   echo "  √ Installing nodenv-update plugin..."
   echo ""
+
   git clone https://github.com/nodenv/nodenv-update.git $HOME/.nodenv/plugins/nodenv-update
+
   echo ""
   echo "  √ Attempting to compile dynamic extensions..."
   echo ""
+
   set +e
   cd $HOME/.nodenv && src/configure && make -C src
   set -e
@@ -49,31 +72,27 @@ else
   echo ""
   echo "  √ Looks like nodenv is already installed. Awesome!"
   echo ""
+
   echo ""
   echo "  √ Installing versions of Node"
   echo ""
+
   for NODE_VERSION in ${NODE_VERSIONS[@]} ; do
+    load_nodenv
     nodenv install -s ${NODE_VERSION}
   done
-fi
-
-# Cache list of already installed global packages
-if command -v npm >/dev/null 2>&1 ; then
-  INSTALLED_PACKAGES=( $(find `npm root -g` -type d -maxdepth 1 -not -path '*/\.*' -print0 | while IFS= read -r -d '' dirname; do echo ${dirname##*/}; done) )
 fi
 
 echo "  √ Running Node install script"
 echo ""
 
 # Check for npm if we’re on OS X
-if command -v npm >/dev/null 2>&1 ; then
-  echo "  √ Looks like npm is installed. Checking for packages to install."
+if command -v yarn >/dev/null 2>&1 ; then
+  echo "  √ Looks like yarn is installed. Checking for packages to install."
   # Install packages globally
   for PACKAGE in ${PACKAGES[@]} ; do
-    if ! echo ${INSTALLED_PACKAGES[@]} | grep -q "${PACKAGE}" ; then
-      npm install -g $PACKAGE
-    fi
+    yarn global add $PACKAGE
   done
 else
-  echo "  ✗ npm is not installed. Skipping..."
+  echo "  ✗ yarn is not installed. Skipping..."
 fi
