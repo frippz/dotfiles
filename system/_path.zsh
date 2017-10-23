ebaba07951071fb8a4b36f4b1223e615ba3ab88f
Fredrik Frodlund <fredrik@frodlund.se>
2017-10-23 10:05:12 +0200

    Remove old npm path

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,2 @@
-export PATH="/usr/local/bin:/usr/local/sbin:$HOME/.scripts:/usr/local/share/npm/bin:/bin:$PATH"
+export PATH="/usr/local/bin:/usr/local/sbin:$HOME/.scripts:/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
