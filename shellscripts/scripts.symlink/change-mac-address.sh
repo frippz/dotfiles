ce4f6515dc6939c2792dbc2e8a5671cd5551dcca
Fredrik Frodlund <fredrik@frodlund.se>
2017-11-04 12:11:51 -0700

    Update MAC address script

--- shellscripts/scripts.symlink/change-mac-address.sh
+++ shellscripts/scripts.symlink/change-mac-address.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-sudo ifconfig en0 ether $(openssl rand -hex 6 | sed 's/\(..\)/\1:/g; s/.$//')
+new_mac="$(openssl rand -hex 6 | sed 's/\(..\)/\1:/g; s/.$//')"
+echo "Setting MAC address of en0 to $new_mac"
+sudo ifconfig en0 ether $new_mac
