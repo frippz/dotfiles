2aa0f6e8b3f388520b9b08419500dadaecaa5188
Fredrik Frodlund <fredrik@frodlund.se>
2018-06-27 13:56:02 +0200

    Match theme with iTerm profile
    …or fall back to hard-coded theme value

--- zsh/theme.zsh
+++ zsh/theme.zsh
@@ -3,4 +3,10 @@
 # Export an environment variable to be used in Neovim etc to set
 # proper themes across the board.
 
-export TERM_THEME="Solarized"
+if [ -n "$ITERM_PROFILE" ]; then
+  # Match theme with iTerm profile
+  export TERM_THEME="$ITERM_PROFILE"
+else
+  # Fall back to hard-coded theme value
+  export TERM_THEME="SolarizedLight"
+fi
