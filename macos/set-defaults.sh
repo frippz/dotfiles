fecaa5c108133f6abb4c39234d8e8070620f0240
Fredrik Frodlund <fredrik@frodlund.se>
2018-07-10 07:43:11 +0200

    Echo what each feature is doing to console

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -10,48 +10,44 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 # General UI/UX                                                               #
 ###############################################################################
 
-# Disable press-and-hold for keys in favor of key repeat.
+echo "✔ Disable press-and-hold for keys in favor of key repeat."
 defaults write -g ApplePressAndHoldEnabled -bool false
 
-# Set a really fast key repeat.
+echo "✔ Set a really fast key repeat."
 defaults write NSGlobalDomain KeyRepeat -int 0
 
-# Disable the sound effects on boot
+echo "✔ Disable the sound effects on boot"
 sudo nvram SystemAudioVolume=" "
 
-# Automatically quit printer app once the print jobs complete
+echo "✔ Automatically quit printer app once the print jobs complete"
 defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
 
-# Automatically quit printer app once the print jobs complete
+echo "✔ Automatically quit printer app once the print jobs complete"
 defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
 
-# Use scroll gesture with the Ctrl (^) modifier key to zoom
+echo "✔ Use scroll gesture with the Ctrl (^) modifier key to zoom"
 defaults write com.apple.universalaccess closeViewScrollWheelToggle -bool true
 defaults write com.apple.universalaccess HIDScrollZoomModifierMask -int 262144
 
-# Follow the keyboard focus while zoomed in
+echo "✔ Follow the keyboard focus while zoomed in"
 defaults write com.apple.universalaccess closeViewZoomFollowsFocus -bool true
 
 ###############################################################################
 # Finder                                                                      #
 ###############################################################################
 
-# Use AirDrop over every interface
+"✔ Use AirDrop over every interface"
 defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
 
 ###############################################################################
 # Google Chrome & Google Chrome Canary                                        #
 ###############################################################################
 
-# Allow installing user scripts via GitHub Gist or Userscripts.org
-defaults write com.google.Chrome ExtensionInstallSources -array "https://gist.githubusercontent.com/" "http://userscripts.org/*"
-defaults write com.google.Chrome.canary ExtensionInstallSources -array "https://gist.githubusercontent.com/" "http://userscripts.org/*"
-
-# Use the system-native print preview dialog
+echo "✔ Use the system-native print preview dialog"
 defaults write com.google.Chrome DisablePrintPreview -bool true
 defaults write com.google.Chrome.canary DisablePrintPreview -bool true
 
-# Expand the print dialog by default
+echo "✔ Expand the print dialog by default"
 defaults write com.google.Chrome PMPrintingExpandedStateForPrint2 -bool true
 defaults write com.google.Chrome.canary PMPrintingExpandedStateForPrint2 -bool true
 
@@ -59,21 +55,21 @@ defaults write com.google.Chrome.canary PMPrintingExpandedStateForPrint2 -bool t
 # Photos                                                                      #
 ###############################################################################
 
-# Don't launch Photos if a device is plugged in
+echo "✔ Don't launch Photos if a device is plugged in"
 defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool true
 
 ###############################################################################
 # Safari                                                                      #
 ###############################################################################
 
-# Set up Safari for development.
+echo "✔ Set up Safari for development."
 defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
 defaults write com.apple.Safari IncludeDevelopMenu -bool true
 defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
 defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
 defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
 
-# Set up Safari Technology Preview for development
+echo "✔ Set up Safari Technology Preview for development"
 defaults write com.apple.SafariTechnologyPreview IncludeInternalDebugMenu -bool true
 defaults write com.apple.SafariTechnologyPreview IncludeDevelopMenu -bool true
 defaults write com.apple.SafariTechnologyPreview WebKitDeveloperExtrasEnabledPreferenceKey -bool true
