18213a9720ae345d9069b48f6efb7c56db592ea1
Fredrik Frodlund <fredrik@frodlund.se>
2018-09-01 15:52:14 +0200

    Don’t lazy load node anymore

--- node/node.zsh
+++ node/node.zsh
@@ -1,47 +1,9 @@
-function load_nodenv() {
-  # Node environment config
-  export NODE_PATH=$HOME/.npm/lib/node_modules
-  export PATH=$PATH:$HOME/.npm/bin
+# Node environment config
+export NODE_PATH=$HOME/.npm/lib/node_modules
+export PATH=$PATH:$HOME/.npm/bin
 
-  # nodenv
-  if [ -d $HOME/.nodenv/bin ] ; then
-    export PATH="$HOME/.nodenv/bin:$PATH"
-    eval "$(nodenv init -)"
-  fi
-}
-
-node() {
-  unset -f node
-  load_nodenv
-  node "$@"
-}
-
-nodenv() {
-  unset -f nodenv
-  load_nodenv
-  nodenv "$@"
-}
-
-nodemon() {
-  unset -f nodemon
-  load_nodenv
-  nodemon "$@"
-}
-
-npm() {
-  unset -f npm
-  load_nodenv
-  npm "$@"
-}
-
-gulp() {
-  unset -f gulp
-  load_nodenv
-  gulp "$@"
-}
-
-nvim() {
-  unset -f nvim
-  load_nodenv
-  nvim "$@"
-}
+# nodenv
+if [ -d $HOME/.nodenv/bin ] ; then
+  export PATH="$HOME/.nodenv/bin:$PATH"
+  eval "$(nodenv init -)"
+fi
