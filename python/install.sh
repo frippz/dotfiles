c9949dfea46a1e40868dac2c146f788e0ebd4418
Fredrik Frodlund <fredrik@frodlund.se>
2018-11-22 10:58:35 +0100

    Change neovim to pynvim for pip and restore python2 pip

--- python/install.sh
+++ python/install.sh
@@ -3,8 +3,45 @@
 # Install pip packages
 
 PIPS=(
-  "neovim"
+  "pynvim"
 )
+
+echo ""
+echo "  ✔ Running pip install script"
+echo ""
+
+# Check for rbenv before attempting to install gems
+if command -v python >/dev/null 2>&1 ; then
+  echo ""
+  echo "   ✔ Upgrading pip"
+  echo ""
+  case "$OSTYPE" in
+    *darwin*)
+      pip install --upgrade pip
+      ;;
+    *linux*)
+      sudo -H pip3 install --upgrade pip
+      ;;
+  esac
+  echo ""
+  echo "   ✔ Installing Python packages"
+  echo ""
+  for PIP in ${PIPS[@]} ; do
+    case "$OSTYPE" in
+      *darwin*)
+        PREFIX=""
+        ;;
+      *linux*)
+        PREFIX="sudo -H "
+        ;;
+    esac
+    $PREFIX pip install -U $PIP
+  done
+else
+  echo ""
+  echo "  ✘ python not found. You may need to install it first."
+  echo ""
+fi
 
 echo ""
 echo "  ✔ Running pip3 install script"
