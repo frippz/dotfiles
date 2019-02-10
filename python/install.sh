248f8042d9081709856431726fa9c1acc4777bcc
Fredrik Frodlund <fredrik@frodlund.se>
2019-02-10 16:15:57 +0100

    Update install script for Python

--- python/install.sh
+++ python/install.sh
@@ -10,7 +10,7 @@ echo ""
 echo "  ✔ Running pip install script"
 echo ""
 
-# Check for rbenv before attempting to install gems
+# Check for Python before attempting to install gems
 if command -v python >/dev/null 2>&1 ; then
   echo ""
   echo "   ✔ Upgrading pip"
@@ -40,42 +40,5 @@ if command -v python >/dev/null 2>&1 ; then
 else
   echo ""
   echo "  ✘ python not found. You may need to install it first."
-  echo ""
-fi
-
-echo ""
-echo "  ✔ Running pip3 install script"
-echo ""
-
-# Check for rbenv before attempting to install gems
-if command -v python3 >/dev/null 2>&1 ; then
-  echo ""
-  echo "   ✔ Upgrading pip3"
-  echo ""
-  case "$OSTYPE" in
-    *darwin*)
-      pip3 install --upgrade pip
-      ;;
-    *linux*)
-      sudo -H pip3 install --upgrade pip
-      ;;
-  esac
-  echo ""
-  echo "   ✔ Installing Python packages"
-  echo ""
-  for PIP in ${PIPS[@]} ; do
-    case "$OSTYPE" in
-      *darwin*)
-        PREFIX=""
-        ;;
-      *linux*)
-        PREFIX="sudo -H "
-        ;;
-    esac
-    $PREFIX pip3 install -U $PIP
-  done
-else
-  echo ""
-  echo "  ✘ python3 not found. You may need to install it first."
   echo ""
 fi
