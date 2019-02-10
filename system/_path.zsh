646aace7106b8697f979a4351ad80e99ff04f8a4
Fredrik Frodlund <fredrik@frodlund.se>
2019-02-10 16:36:29 +0100

    Add user pip path

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,2 @@
-export PATH="/usr/local/bin:/usr/local/sbin:/usr/local/opt/python/libexec/bin:$HOME/.scripts:/bin:$PATH"
+export PATH="/usr/local/bin:/usr/local/sbin:/usr/local/opt/python/libexec/bin:$HOME/.scripts:$HOME/Library/Python/3.7/bin:/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
