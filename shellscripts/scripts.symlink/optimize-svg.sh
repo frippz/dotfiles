5d19b4644aae905240d8a0c2d05e1036659911de
Fredrik Frodlund <fredrik@frodlund.se>
2019-09-19 10:54:43 +0200

    Don't SVG script of svgo isn't installed

--- shellscripts/scripts.symlink/optimize-svg.sh
+++ shellscripts/scripts.symlink/optimize-svg.sh
@@ -1,15 +1,23 @@
 #!/bin/sh
 
-BASEDIR=$(dirname $0)
-svgoConf='svgo_conf.yml'
+if command -v svgo ; then
 
-svgPath=$1
+  BASEDIR=$(dirname $0)
+  svgoConf='svgo_conf.yml'
 
-if [[ -n "$svgPath" ]]; then
-  svgo -f "$svgPath" -o "$svgPath" --pretty --multipass --config=${BASEDIR}/$svgoConf
+  svgPath=$1
+
+  if [[ -n "$svgPath" ]]; then
+    svgo -f "$svgPath" -o "$svgPath" --pretty --multipass --config=${BASEDIR}/$svgoConf
+  else
+    echo "Usage:"
+    echo "svgo.sh path/to/svg/folder"
+    echo
+    echo "Note: this script only handles entire folders. Do not enter direct file paths."
+  fi
+
 else
-  echo "Usage:"
-  echo "svgo.sh path/to/svg/folder"
-  echo
-  echo "Note: this script only handles entire folders. Do not enter direct file paths."
+
+  echo "Error. svgo not found."
+
 fi
