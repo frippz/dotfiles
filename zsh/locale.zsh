66cd8da5eb6fb346daf756b79f412bcdb89a81a8
Fredrik Frodlund <fredrik@frodlund.se>
2020-05-21 06:37:19 +0000

    Handle locales differently in macOS & Linux

--- zsh/locale.zsh
+++ zsh/locale.zsh
@@ -1,2 +1,9 @@
-export LANG=sv_SE.UTF-8
-export LC_ALL=en_US.UTF-8
+case "$OSTYPE" in
+  *darwin*)
+    export LANG=sv_SE.UTF-8
+    export LC_ALL=en_US.UTF-8
+    ;;
+  linux*)
+    export LC_ALL=en_US.UTF-8
+    ;;
+esac
