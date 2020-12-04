03336fcd96be3c73b09c20c97272924cac313702
Fredrik Frodlund <fredrik@frodlund.se>
2020-12-04 16:26:07 +0100

    Add alias for cleaning up branches

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -23,6 +23,7 @@ alias gb='git branch'
 alias gba='git branch -a'
 alias gbr='git branch --remote'
 alias gbvv='git branch -vv'
+alias gbc='git fetch --prune && git branch -D !master'
 
 # Commit
 alias gc='git commit -v'
