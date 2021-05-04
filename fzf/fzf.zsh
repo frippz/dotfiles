db4c87b6bfd2de44783eb999ceea7ef067cfa0d7
Fredrik Frodlund <fredrik@frodlund.se>
2021-05-04 15:44:19 +0200

    Add support for different BAT_THEME variables

--- fzf/fzf.zsh
+++ fzf/fzf.zsh
@@ -5,3 +5,9 @@ fi
 export FZF_DEFAULT_COMMAND='ag --nocolor -g ""'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_ALT_C_COMMAND="$FZF_DEFAULT_COMMAND"
+
+if [ "$TERM_THEME" = "SolarizedLight" ]; then
+  export BAT_THEME="Solarized (light)"
+else
+  export BAT_THEME="gruvbox-dark"
+fi
