eabe07429f635e7cde6f6985e2bcbc500f68d282
Fredrik Frodlund <fredrik@frodlund.se>
2021-11-15 20:46:04 +0100

    Support Homebrew prefix for Apple Silicon

--- zsh/zsh-syntax-highlighting.zsh
+++ zsh/zsh-syntax-highlighting.zsh
@@ -1,3 +1,10 @@
-if [ -f /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ]; then
-  source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+# Check if HOMEBREW_PREFIX is set
+if [[ -z "${HOMEBREW_PREFIX}" ]]; then
+  PREFIX="/usr/local"
+else
+  PREFIX="${HOMEBREW_PREFIX}"
+fi
+
+if [ -f $PREFIX/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ]; then
+  source $PREFIX/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 fi
