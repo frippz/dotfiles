4719f638f45d38ad717fe4f3d18a1359bb1c2bdb
Fredrik Frodlund <fredrik@frodlund.se>
2021-11-19 11:00:34 +0100

    Fix HOMEBREW_PREFIX for gcloud completion

--- gcloud/completion.zsh
+++ gcloud/completion.zsh
@@ -1,4 +1,11 @@
-if [ -d "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/" ]; then
-  source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc'
-  source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc'
+# Check if HOMEBREW_PREFIX is set
+if [[ -z "${HOMEBREW_PREFIX}" ]]; then
+  PREFIX="/usr/local"
+else
+  PREFIX="${HOMEBREW_PREFIX}"
+fi
+
+if [ -d "$PREFIX/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/" ]; then
+  source $PREFIX/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc
+  source $PREFIX/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc
 fi
