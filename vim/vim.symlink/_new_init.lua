25716ec0ac35709c8e367b5424c11ff802c10f6e
Fredrik Frodlund <fredrik@frodlund.se>
2022-04-19 20:18:54 +0200

    Begin migration to init.lua

---
+++ vim/vim.symlink/_new_init.lua
@@ -0,0 +1,20 @@
+-- Plugins
+-- ============================================================================
+local paq = require("paq")
+
+paq({
+
+  -- package manager
+	"savq/paq-nvim",
+
+  -- color themes
+  "gruvbox-community/gruvbox"
+  "sonph/onehalf"
+
+})
+
+-- Mappings
+-- ============================================================================
+
+-- Set leader
+vim.g.mapleader = ","
