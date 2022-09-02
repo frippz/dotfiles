3b86bbd3dec10640350d7c83868efb431e7d52a4
Fredrik Frodlund <fredrik@frodlund.se>
2022-09-02 21:27:14 +0200

    Add install scripts and fix paq-nvim check

--- vim/install.sh
+++ vim/install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Install paq-nvim
-git clone --depth=1 https://github.com/savq/paq-nvim.git "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/pack/paqs/start/paq-nvim
-
-mkdir -p ${XDG_CONFIG_HOME:=$HOME/.config}
-ln -sf ~/.vim $XDG_CONFIG_HOME/nvim
-# ln -sf ~/.vimrc $XDG_CONFIG_HOME/nvim/init.vim
+if [ ! -d ${XDG_DATA_HOME:-$HOME/.local/share} ]; then
+  git clone --depth=1 https://github.com/savq/paq-nvim.git "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/pack/paqs/start/paq-nvim
+else
+  echo "paq-nvim is already present. Skipping..."
+fi
