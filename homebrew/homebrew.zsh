3fdf5fba97409f5d619bf994b114442a58dbc240
Fredrik Frodlund <fredrik@frodlund.se>
2023-01-03 19:48:21 +0100

    Try running each install script in a zsh command

--- homebrew/homebrew.zsh
+++ homebrew/homebrew.zsh
@@ -1,3 +1,11 @@
 export HOMEBREW_BUNDLE_NO_LOCK=1
 export HOMEBREW_NO_ENV_HINTS=1
 export HOMEBREW_BUNDLE_FILE=$HOME/.dotfiles/homebrew/Brewfile
+
+# Homebrew completion
+if command -v brew &>/dev/null; then
+  FPATH=$(brew --prefix)/share/zsh/site-functions:$FPATH
+
+  autoload -Uz compinit
+  compinit
+fi
