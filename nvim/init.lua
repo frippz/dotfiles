89d09e9c0a9487b464a5ba2a2e9a94dea7c11c81
Fredrik Frodlund <fredrik@frodlund.se>
2023-03-16 19:16:58 +0100

    Change config for Lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -207,45 +207,6 @@ nvimCmd([[
 -- Plugins
 -- ============================================================================
 
--- null-ls.nvim
--- ----------------------------------------------------------------------------
-local null_ls = require("null-ls")
-
-local group = vim.api.nvim_create_augroup("lsp_format_on_save", { clear = false })
-local event = "BufWritePre" -- or "BufWritePost"
-local async = event == "BufWritePost"
-
-null_ls.setup({
-  sources = {
-    null_ls.builtins.formatting.prettierd,
-    null_ls.builtins.formatting.beautysh,
-  },
-  on_attach = function(client, bufnr)
-    if client.supports_method("textDocument/formatting") then
-      vim.keymap.set("n", "<Leader>f", function()
-        vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
-      end, { buffer = bufnr, desc = "[lsp] format" })
-
-      -- format on save
-      vim.api.nvim_clear_autocmds({ buffer = bufnr, group = group })
-      vim.api.nvim_create_autocmd(event, {
-        buffer = bufnr,
-        group = group,
-        callback = function()
-          vim.lsp.buf.format({ bufnr = bufnr, async = async })
-        end,
-        desc = "[lsp] format on save",
-      })
-    end
-
-    if client.supports_method("textDocument/rangeFormatting") then
-      vim.keymap.set("x", "<Leader>f", function()
-        vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
-      end, { buffer = bufnr, desc = "[lsp] format" })
-    end
-  end,
-})
-
 -- mason.nvim
 -- ----------------------------------------------------------------------------
 local mason_null_ls = require("mason-null-ls")
@@ -270,7 +231,7 @@ mason_null_ls.setup({
     "html-lsp",
     "lua-language-server",
     "markdownlint",
-    "prettierd",
+    "prettier",
     "rubocop",
     "stylelint-lsp",
     "svelte-language-server",
@@ -302,6 +263,11 @@ lspconfig.eslint.setup({
       command = "EslintFixAll",
     })
   end,
+})
+
+-- bash
+lspconfig.bashls.setup({
+  capabilities = capabilities,
 })
 
 -- cssls
