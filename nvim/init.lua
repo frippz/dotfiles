21429ecc7120fad269d24705b585e62340de11b6
Fredrik Frodlund <fredrik@frodlund.se>
2023-04-10 14:14:57 +0200

    chore: remove old theme specific config

--- nvim/init.lua
+++ nvim/init.lua
@@ -147,15 +147,12 @@ nvimCmd("syntax enable")
 nvimCmd("highlight! link SignColumn LineNr")
 nvimCmd("hi Normal guibg=NONE ctermbg=NONE")
 nvimCmd("highlight Comment cterm=italic gui=italic")
+g.everforest_transparent_background = 1
 
 -- Dark
 -- ----------------------------------------------------------------------------
 function ThemeDark()
   o.background = "dark"
-  g.gruvbox_material_better_performance = 1
-  g.gruvbox_material_enable_bold = 1
-  g.gruvbox_material_dim_inactive_windows = 0
-  g.gruvbox_material_transparent_background = "1"
   nvimCmd("colorscheme everforest")
 end
 
