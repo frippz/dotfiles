369ab74691b4deff99d9cc18f26b1eb3920aba08
Fredrik Frodlund <fredrik@frodlund.se>
2023-04-23 14:29:23 +0200

    chore: set up default_on_attach for nvim-tree
    Now we don’t have to configure every damn mapping again

--- nvim/init.lua
+++ nvim/init.lua
@@ -371,14 +371,9 @@ local function on_attach(bufnr)
     return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
   end
 
+  api.config.mappings.default_on_attach(bufnr)
+
   vim.keymap.set("n", "<C-o>", api.tree.change_root_to_node, opts("CD"))
-  vim.keymap.set("n", "<CR>", api.node.open.edit, opts("Open"))
-  vim.keymap.set("n", "m", api.marks.toggle, opts("Toggle Bookmark"))
-  vim.keymap.set("n", "p", api.fs.paste, opts("Paste"))
-  vim.keymap.set("n", "r", api.fs.rename, opts("Rename"))
-  vim.keymap.set("n", "R", api.tree.reload, opts("Refresh"))
-  vim.keymap.set("n", "x", api.fs.cut, opts("Cut"))
-  vim.keymap.set("n", "a", api.fs.create, opts("Create"))
 end
 
 require("nvim-tree").setup({
