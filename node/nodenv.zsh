c9017b800b2ca6981d651d7c2902ed3887f53ea6
Fredrik Frodlund <fredrik@frodlund.se>
2023-05-10 09:19:53 +0000

    chore(node): update nodenv path check for linux

--- node/nodenv.zsh
+++ node/nodenv.zsh
@@ -1,3 +1,12 @@
 if [ -d $HOME/.nodenv ]; then
-  eval "$(nodenv init -)"
+  case $OSTYPE in 
+    *darwin*)
+      eval "$(nodenv init -)"
+      ;;
+    linux*)
+      if [ -d $HOME/.nodenv/bin ]; then
+        PATH=$HOME/.nodenv/bin:$PATH
+        eval "$(nodenv init -)"
+      fi
+  esac
 fi
