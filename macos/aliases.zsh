f2157a8b2da7c162135b6127d7354543cb88e422
Fredrik Frodlund <fredrik@frodlund.se>
2023-05-19 14:50:21 +0200

    chore(macos): remove defunct alias

--- macos/aliases.zsh
+++ macos/aliases.zsh
@@ -5,8 +5,5 @@ case "$OSTYPE" in
   *darwin*)
     # Clean up "Open with…" menu
     alias fixopenwith="/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user"
-
-    # Start OS X screensaver
-    alias screensaver-launch="open /System/Library/Frameworks/ScreenSaver.framework/Versions/Current/Resources/ScreenSaverEngine.app"
     ;;
 esac
