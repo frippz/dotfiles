a9bd0b97929b03dd2821b9ae69d95f74285c4afc
Fredrik Frodlund <fredrik@frodlund.se>
2023-11-14 15:57:19 +0100

    chore: update gcloud path & completion

--- gcloud/completion.zsh
+++ gcloud/completion.zsh
@@ -1,11 +1,4 @@
-# Check if HOMEBREW_PREFIX is set
-if [[ -z "${HOMEBREW_PREFIX}" ]]; then
-  PREFIX="/usr/local"
-else
-  PREFIX="${HOMEBREW_PREFIX}"
-fi
-
-if [ -d "$PREFIX/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/" ]; then
-  source $PREFIX/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc
-  source $PREFIX/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc
+if [ -d "$(brew --prefix)/share/google-cloud-sdk/" ]; then
+  source "$(brew --prefix)/share/google-cloud-sdk/path.zsh.inc"
+  source "$(brew --prefix)/share/google-cloud-sdk/completion.zsh.inc"
 fi
