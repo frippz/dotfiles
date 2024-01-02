61312ad6fbeaca42f828e3841567640575b43ecc
Fredrik Frodlund <fredrik@frodlund.se>
2024-01-02 15:55:42 +0100

    fix: add checks for paths and binaries

--- gcloud/completion.zsh
+++ gcloud/completion.zsh
@@ -1,4 +1,6 @@
-if [ -d "$(brew --prefix)/share/google-cloud-sdk/" ]; then
-  source "$(brew --prefix)/share/google-cloud-sdk/path.zsh.inc"
-  source "$(brew --prefix)/share/google-cloud-sdk/completion.zsh.inc"
+if command -v brew > /dev/null 2>&1; then
+  if [ -d "$(brew --prefix)/share/google-cloud-sdk/" ]; then
+    source "$(brew --prefix)/share/google-cloud-sdk/path.zsh.inc"
+    source "$(brew --prefix)/share/google-cloud-sdk/completion.zsh.inc"
+  fi
 fi
