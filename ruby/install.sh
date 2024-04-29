9714d47e2d9c2e85e44ce9b2d6dc278e71a41383
Fredrik Frodlund <fredrik@frodlund.se>
2024-04-29 12:04:01 +0200

    feat(ruby): implement msg functions in install script

--- ruby/install.sh
+++ ruby/install.sh
@@ -1,5 +1,7 @@
-#!/usr/bin/env bash
-#
+#!/usr/bin/env zsh
+
+source $HOME/.dotfiles/zsh/msg.zsh
+
 # Install Ruby via rbenv and gems
 
 VERSION="3.2.2"
@@ -20,9 +22,7 @@ RBENV_HOME="$HOME/.rbenv"
 # Install rbenv
 if command -v git > /dev/null 2>&1; then
   if [ ! -d "$RBENV_HOME" ]; then
-    echo ""
-    echo " ✅ Installing rbenv using git"
-    echo ""
+    msg_info "Installing rbenv using git"
 
     git clone https://github.com/rbenv/rbenv.git $RBENV_HOME
     PATH=$HOME/.rbenv/bin:$PATH
@@ -30,19 +30,16 @@ if command -v git > /dev/null 2>&1; then
     git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build
 
   else
-    echo ""
-    echo " ⚠️  $RBENV_HOME is already present. Skipping install..."
-    echo ""
+    msg_done "$RBENV_HOME is already present. Skipping install..."
   fi
 else
-  echo "  ⛔️ git not found. Aborting..."
+  msg_fail "git not found. Aborting..."
+  exit 1
 fi
 
 if command -v rbenv > /dev/null 2>&1; then
 
-  echo ""
-  echo " ✅ Install Ruby using rbenv and set ${VERSION} as global"
-  echo ""
+  msg_info "Installing Ruby using rbenv and set ${VERSION} as global"
 
   rbenv install ${VERSION} --skip-existing
   rbenv global ${VERSION}
@@ -52,16 +49,14 @@ fi
 
 # Install rbenv plugins
 if [ -d $RBENV_HOME ]; then
-  echo ""
-  echo " ✅ Installing rbenv plugins"
-  echo ""
+  msg_info "Installing rbenv plugins"
   for PLUGIN in ${PLUGINS[@]}; do
     PLUGIN_NAME=$(echo $PLUGIN | sed -E 's/.*\/([^/]+)\.git$/\1/')
     if [ ! -d "$(rbenv root)/plugins/$PLUGIN_NAME" ]; then
+      msg_info "Installing $PLUGIN"
       git clone $PLUGIN $(rbenv root)/plugins/$PLUGIN_NAME
     else
-      echo "    ℹ️  $PLUGIN_NAME is already installed"
-      echo ""
+      msg_done "$PLUGIN_NAME is already installed."
     fi
   done
 fi
@@ -69,24 +64,21 @@ fi
 # Check for gem before attempting to install packages
 if command -v gem > /dev/null 2>&1; then
 
-  echo ""
-  echo " ✅ Installing gems"
-  echo ""
+  msg_info "Installing gems"
 
   for GEM in ${GEMS[@]}; do
     if ! gem list | grep -q "$GEM"; then
+      msg_info "Installing $GEM"
       gem install $GEM
     else
-      echo "    ℹ️  $GEM already installed. Checking for update instead"
-      echo ""
+      msg_done "$GEM already installed. Checking for update instead."
       gem update $GEM
     fi
   done
 
 else
 
-  echo ""
-  echo " ⛔️ gem command not found."
-  echo ""
+  msg_fail "gem command not found."
+  exit 1
 
 fi
