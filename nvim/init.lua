a817969810df01863c6d9f106bb5a71127af3cf7
Fredrik Frodlund <fredrik@frodlund.se>
2024-05-05 17:58:24 +0200

    feat(nvim): add notify & noice

--- nvim/init.lua
+++ nvim/init.lua
@@ -354,6 +354,9 @@ telescope.setup({
   },
 })
 
+-- extensions
+telescope.load_extension("noice")
+
 -- telescope mappings
 map("n", "<leader>ft", ":Telescope<CR>", mapOpts)
 map("n", "<C-p>", ":Telescope find_files hidden=true<CR>", mapOpts)
@@ -460,3 +463,30 @@ require("FTerm").setup({})
 
 -- mappings
 map("n", "<Leader>t", ":lua require('FTerm').toggle()<CR>", mapOpts)
+
+-- notify.nvim
+-- ----------------------------------------------------------------------------
+require("notify").setup({
+  background_colour = "#000000",
+})
+
+-- noice.nvim
+-- ----------------------------------------------------------------------------
+require("noice").setup({
+  lsp = {
+    -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+    override = {
+      ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+      ["vim.lsp.util.stylize_markdown"] = true,
+      ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+    },
+  },
+  -- you can enable a preset for easier configuration
+  presets = {
+    bottom_search = true, -- use a classic bottom cmdline for search
+    command_palette = true, -- position the cmdline and popupmenu together
+    long_message_to_split = true, -- long messages will be sent to a split
+    inc_rename = false, -- enables an input dialog for inc-rename.nvim
+    lsp_doc_border = false, -- add a border to hover docs and signature help
+  },
+})
