71c1c8d6556ca7b1c8676111943fc1b72f899d0b
Fredrik Frodlund <fredrik@frodlund.se>
2024-05-26 07:54:13 +0200

    feat(eza): no quotes on folders with spaces

--- eza/aliases.zsh
+++ eza/aliases.zsh
@@ -4,8 +4,8 @@ if command -v eza >/dev/null 2>&1 ; then
   # Hijack ls
   alias ls="eza"
   alias l="eza -1"
-  alias ll="eza --long --header --time-style=relative --no-permissions"
-  alias llp="eza --long --header --time-style=relative"
+  alias ll="eza --long --header --time-style=relative --no-quotes --no-permissions"
+  alias llp="eza --long --header --time-style=relative --no-quotes"
   alias la="ll -a"
   alias lap="eza --long -a"
   alias lt="ll --tree --level=4"
