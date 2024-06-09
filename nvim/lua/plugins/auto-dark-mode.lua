f9fef47d6549fab9df54f8350ff2b39e62695765
Fredrik Frodlund <fredrik@frodlund.se>
2024-06-09 22:16:18 +0200

    refactor(nvim): move auto-dark-mode config to plugin

--- nvim/lua/plugins/auto-dark-mode.lua
+++ nvim/lua/plugins/auto-dark-mode.lua
@@ -1,3 +1,17 @@
 return {
   "f-person/auto-dark-mode.nvim",
+  config = function()
+    local auto_dark_mode = require("auto-dark-mode")
+    auto_dark_mode.setup({
+      update_interval = 1000,
+      set_dark_mode = function()
+        ThemeDark()
+      end,
+      set_light_mode = function()
+        ThemeLight()
+      end,
+    })
+
+    auto_dark_mode.init()
+  end,
 }
--- nvim/lua/themes.lua
+++ nvim/lua/themes.lua
@@ -1,5 +1,3 @@
--- Themes
--- ============================================================================
 nvimCmd("syntax enable")
 nvimCmd("highlight! link SignColumn LineNr")
 nvimCmd("hi Normal guibg=NONE ctermbg=NONE")
@@ -7,34 +5,16 @@ nvimCmd("highlight Comment cterm=italic gui=italic")
 g.everforest_transparent_background = 1
 
 -- Dark
--- ----------------------------------------------------------------------------
 function ThemeDark()
   o.background = "dark"
   vim.cmd.colorscheme("everforest")
 end
 
 -- Light
--- ----------------------------------------------------------------------------
 function ThemeLight()
   o.background = "light"
   vim.cmd.colorscheme("everforest")
 end
-
--- Auto Dark Mode
--- ----------------------------------------------------------------------------
-local auto_dark_mode = require("auto-dark-mode")
-
-auto_dark_mode.setup({
-  update_interval = 1000,
-  set_dark_mode = function()
-    ThemeDark()
-  end,
-  set_light_mode = function()
-    ThemeLight()
-  end,
-})
-
-auto_dark_mode.init()
 
 -- Theming for Linux
 -- Set theme based on $TERM_THEME
