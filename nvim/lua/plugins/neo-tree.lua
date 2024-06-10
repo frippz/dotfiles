173cf3952887a14e80fdc111934b85d3a4d88826
Fredrik Frodlund <fredrik@frodlund.se>
2024-06-10 10:46:07 +0200

    feat(nvim): use native file system for changes

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -18,6 +18,7 @@ return {
   },
   config = function()
     require("neo-tree").setup({
+      use_libuv_file_watcher = true,
       filesystem = {
         filtered_items = {
           hide_dotfiles = false,
