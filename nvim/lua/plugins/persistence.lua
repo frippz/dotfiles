b0c12f66b30f1893b269178cc7b0ebe1d71b0dce
Fredrik Frodlund <fredrik@frodlund.se>
2024-06-19 19:19:52 +0200

    fix(nvim): set up keymap for persistence

--- nvim/lua/plugins/persistence.lua
+++ nvim/lua/plugins/persistence.lua
@@ -1,4 +1,13 @@
 return {
   "folke/persistence.nvim",
   event = "BufReadPre",
+  config = function()
+    require("persistence").setup({})
+  end,
+  keys = {
+    {
+      "<Leader>sr",
+      ":lua require('persistence').load({ last = true })<cr>",
+    },
+  },
 }
