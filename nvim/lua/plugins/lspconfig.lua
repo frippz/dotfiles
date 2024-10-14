3725da6b79f427bb0b0c161ee718aacfae4ff159
Fredrik Frodlund <fredrik@frodlund.se>
2024-10-14 14:46:48 +0200

    feat(nvim): add LSP support for Deno 🦕

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -72,6 +72,14 @@ return {
     -- typescript
     lspconfig.tsserver.setup({
       capabilities = capabilities,
+      root_dir = lspconfig.util.root_pattern("package.json"),
+      single_file_support = false,
+    })
+
+    -- deno
+    lspconfig.denols.setup({
+      capabilities = capabilities,
+      root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc"),
     })
 
     -- eslint
