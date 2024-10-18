eb5a17c44857a5b047568c0e9d9d067e5b07b58c
Fredrik Frodlund <fredrik@frodlund.se>
2024-10-18 15:13:09 +0200

    chore(macos): let shfmt do it’s thing

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -4,7 +4,11 @@
 sudo -v
 
 # Keep-alive: update existing `sudo` time stamp until script has finished
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+while true; do
+  sudo -n true
+  sleep 60
+  kill -0 "$$" || exit
+done 2> /dev/null &
 
 # General UI/UX
 echo " ✅ Disable press-and-hold for keys in favor of key repeat."
