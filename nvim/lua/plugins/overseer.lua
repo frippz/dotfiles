36e5a11980541bb63f9e326ee3c9568a34e3a476
Fredrik Frodlund <fredrik@frodlund.se>
2024-11-25 08:31:13 +0100

    feat(nvim): add overseer plugin

---
+++ nvim/lua/plugins/overseer.lua
@@ -0,0 +1,16 @@
+return {
+  "stevearc/overseer.nvim",
+  keys = {
+    {
+      "<leader>rr",
+      "<cmd>OverseerRun<cr>",
+      desc = "OverseerRun",
+    },
+    {
+      "<leader>rt",
+      "<cmd>OverseerToggle<cr>",
+      desc = "OverseerToggle",
+    },
+  },
+  opts = {},
+}
