13dd35da60f1e3112257362215b4948bfb516878
Fredrik Frodlund <fredrik@frodlund.se>
2025-01-14 17:04:03 +0100

    feat(nvim): add more shortcuts to dashboard

--- nvim/lua/plugins/dashboard.lua
+++ nvim/lua/plugins/dashboard.lua
@@ -23,15 +23,44 @@ return {
         },
         center = {
           {
-            icon = "🔄 ",
+            icon = "♻️  ",
             icon_hl = "Title",
             desc = "Restore previous session",
             desc_hl = "String",
-            key = "b",
-            keymap = "R",
+            key = "r",
             key_hl = "Number",
             key_format = " %s", -- remove default surrounding `[]`
             action = "lua require('persistence').load()",
+          },
+          {
+            icon = "💤 ",
+            icon_hl = "Title",
+            desc = "Lazy",
+            desc_hl = "String",
+            key = "l",
+            key_hl = "Number",
+            key_format = " %s", -- remove default surrounding `[]`
+            action = "Lazy",
+          },
+          {
+            icon = "⚙️  ",
+            icon_hl = "Title",
+            desc = "Mason",
+            desc_hl = "String",
+            key = "m",
+            key_hl = "Number",
+            key_format = " %s", -- remove default surrounding `[]`
+            action = "Mason",
+          },
+          {
+            icon = "🔭 ",
+            icon_hl = "Title",
+            desc = "Telescope",
+            desc_hl = "String",
+            key = "t",
+            key_hl = "Number",
+            key_format = " %s", -- remove default surrounding `[]`
+            action = "Telescope",
           },
         },
         -- footer = { "footer" },
