c7ff7cf4f52028b2106ba3aab3a4b392863c6b40
Fredrik Frodlund <fredrik@frodlund.se>
2025-02-24 11:50:45 +0100

    feat(nvim): add shortcut keys for dashboard

--- nvim/lua/plugins/dashboard.lua
+++ nvim/lua/plugins/dashboard.lua
@@ -27,6 +27,7 @@ return {
             icon_hl = "Title",
             desc = "Restore previous session",
             desc_hl = "String",
+            key = "r",
             action = "lua require('persistence').load()",
           },
           {
@@ -34,6 +35,7 @@ return {
             icon_hl = "Title",
             desc = "Overseer",
             desc_hl = "String",
+            key = "o",
             action = "require('overseer').run_template()",
           },
           {
@@ -41,6 +43,7 @@ return {
             icon_hl = "Title",
             desc = "Lazy",
             desc_hl = "String",
+            key = "l",
             action = "Lazy",
           },
           {
@@ -48,6 +51,7 @@ return {
             icon_hl = "Title",
             desc = "Mason",
             desc_hl = "String",
+            key = "m",
             action = "Mason",
           },
           {
@@ -55,6 +59,7 @@ return {
             icon_hl = "Title",
             desc = "Telescope",
             desc_hl = "String",
+            key = "t",
             action = "Telescope",
           },
           {
@@ -62,6 +67,7 @@ return {
             icon_hl = "Title",
             desc = "Quit",
             desc_hl = "String",
+            key = "q",
             action = "q",
           },
         },
