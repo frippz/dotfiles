651b5ab7ce51b56404242550923f06a605d634e2
Fredrik Frodlund <fredrik@frodlund.se>
2025-04-20 17:53:23 +0200

    chore(nvim): modernize auto-dark-mode config

--- nvim/lua/plugins/auto-dark-mode.lua
+++ nvim/lua/plugins/auto-dark-mode.lua
@@ -4,18 +4,14 @@
 return {
   "f-person/auto-dark-mode.nvim",
 
-  config = function()
-    local auto_dark_mode = require("auto-dark-mode")
-    auto_dark_mode.setup({
-      update_interval = 1000,
-      set_dark_mode = function()
-        ThemeDark()
-      end,
-      set_light_mode = function()
-        ThemeLight()
-      end,
-    })
-
-    auto_dark_mode.init()
-  end,
+  opts = {
+    fallback = "light",
+    update_interval = 1000,
+    set_dark_mode = function()
+      ThemeDark()
+    end,
+    set_light_mode = function()
+      ThemeLight()
+    end,
+  },
 }
