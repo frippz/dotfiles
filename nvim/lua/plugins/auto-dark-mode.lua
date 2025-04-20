690fe953868a6fee125575a2b6df34390148f9c8
Fredrik Frodlund <fredrik@frodlund.se>
2025-04-20 16:31:21 +0000

    fix(nvim): disable auto-dark-mode on Linux

--- nvim/lua/plugins/auto-dark-mode.lua
+++ nvim/lua/plugins/auto-dark-mode.lua
@@ -1,17 +1,21 @@
 -- A Neovim plugin for macOS, Linux, and Windows that automatically changes the
 -- editor appearance based on system settings.
 
-return {
-  "f-person/auto-dark-mode.nvim",
+if not vim.fn.has("Linux") then
+  return {
+    "f-person/auto-dark-mode.nvim",
 
-  opts = {
-    fallback = "light",
-    update_interval = 1000,
-    set_dark_mode = function()
-      ThemeDark()
-    end,
-    set_light_mode = function()
-      ThemeLight()
-    end,
-  },
-}
+    opts = {
+      fallback = "light",
+      update_interval = 1000,
+      set_dark_mode = function()
+        ThemeDark()
+      end,
+      set_light_mode = function()
+        ThemeLight()
+      end,
+    },
+  }
+else
+  return {}
+end
