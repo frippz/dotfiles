e62080a904c67baa24ab19a06735988cd9a427f2
Fredrik Frodlund <fredrik@frodlund.se>
2025-08-18 13:50:29 +0200

    feat(nvim): add custom action to snacks.nvim file picker

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -11,6 +11,63 @@ return {
     indent = { enabled = true },
     input = { enabled = true },
     picker = { enabled = true },
+    picker = {
+      enabled = true,
+
+      sources = {
+        explorer = {
+          actions = {
+
+            -- Custom action to copy file path in file picker
+            copy_file_path = {
+              action = function(_, item)
+                if not item then
+                  return
+                end
+
+                local vals = {
+                  ["BASENAME"] = vim.fn.fnamemodify(item.file, ":t:r"),
+                  ["EXTENSION"] = vim.fn.fnamemodify(item.file, ":t:e"),
+                  ["FILENAME"] = vim.fn.fnamemodify(item.file, ":t"),
+                  ["PATH"] = item.file,
+                  ["PATH (CWD)"] = vim.fn.fnamemodify(item.file, ":."),
+                  ["PATH (HOME)"] = vim.fn.fnamemodify(item.file, ":~"),
+                  ["URI"] = vim.uri_from_fname(item.file),
+                }
+
+                local options = vim.tbl_filter(function(val)
+                  return vals[val] ~= ""
+                end, vim.tbl_keys(vals))
+                if vim.tbl_isempty(options) then
+                  vim.notify("No values to copy", vim.log.levels.WARN)
+                  return
+                end
+                table.sort(options)
+                vim.ui.select(options, {
+                  prompt = "Choose to copy to clipboard:",
+                  format_item = function(list_item)
+                    return ("%s: %s"):format(list_item, vals[list_item])
+                  end,
+                }, function(choice)
+                  local result = vals[choice]
+                  if result then
+                    vim.fn.setreg("+", result)
+                    Snacks.notify.info("Yanked `" .. result .. "`")
+                  end
+                end)
+              end,
+            },
+          },
+          win = {
+            list = {
+              keys = {
+                ["y"] = "copy_file_path",
+              },
+            },
+          },
+        },
+      },
+    },
     notifier = { enabled = true },
     quickfile = { enabled = true },
     scope = { enabled = true },
