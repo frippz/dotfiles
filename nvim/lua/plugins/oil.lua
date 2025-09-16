edb78d28678804d5883e3211ed6b9e690d848772
Fredrik Frodlund <fredrik@frodlund.se>
2025-09-16 20:37:57 +0200

    fix: improve config for oil.nvim

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,10 +1,30 @@
 return {
   "stevearc/oil.nvim",
 
-  dependencies = { { "echasnovski/mini.icons" } },
-  lazy = false,
-
   ---@module 'oil'
   ---@type oil.SetupOpts
-  opts = {},
+  dependencies = {
+    {
+      "nvim-mini/mini.icons",
+      opts = {},
+    },
+  },
+  lazy = false,
+
+  opts = {
+    columns = {
+      "icon",
+      -- "permissions",
+      -- "size",
+      -- "mtime",
+    },
+  },
+
+  keys = {
+    {
+      "<Leader>e",
+      ":Oil<CR>",
+      desc = "Open file explorer (oil.nvim)",
+    },
+  },
 }
