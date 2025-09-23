d39adf251550b9f8ec64332a90a5d6f88fafede0
Fredrik Frodlund <fredrik@frodlund.se>
2025-09-23 10:40:00 +0200

    feat(homebrew): use update-if-needed to speed things up

--- homebrew/aliases.zsh
+++ homebrew/aliases.zsh
@@ -1,6 +1,6 @@
-if command -v brew >/dev/null 2>&1 ; then
+if command -v brew >/dev/null 2>&1; then
   alias brews='brew list --versions'
-  alias bubo='brew update && brew outdated'
+  alias bubo='brew update-if-needed && brew outdated'
   alias bubc='brew upgrade && brew cleanup'
   alias bubu='bubo && bubc'
   alias bi='brew install'
