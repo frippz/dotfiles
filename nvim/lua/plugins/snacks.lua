138aa028d456cb1df7c8185968a527d6fee0a7c6
Fredrik Frodlund <fredrik@frodlund.se>
2025-11-01 19:04:45 +0100

    feat(nvim): enable GH in Snacks.nvim

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -110,6 +110,11 @@ return {
       enabled = true,
     },
 
+    -- GitHub CLI
+    gh = {
+      enabled = true,
+    },
+
     -- Indent
     indent = {
       enabled = true,
@@ -271,6 +276,36 @@ return {
       "<leader>D",
       ":lua Snacks.picker.diagnostics()<cr>",
       desc = "Buffer diagnostics",
+    },
+
+    -- GitHub
+    {
+      "<leader>gi",
+      function()
+        Snacks.picker.gh_issue()
+      end,
+      desc = "GitHub Issues (open)",
+    },
+    {
+      "<leader>gI",
+      function()
+        Snacks.picker.gh_issue({ state = "all" })
+      end,
+      desc = "GitHub Issues (all)",
+    },
+    {
+      "<leader>gp",
+      function()
+        Snacks.picker.gh_pr()
+      end,
+      desc = "GitHub Pull Requests (open)",
+    },
+    {
+      "<leader>gP",
+      function()
+        Snacks.picker.gh_pr({ state = "all" })
+      end,
+      desc = "GitHub Pull Requests (all)",
     },
   },
 }
