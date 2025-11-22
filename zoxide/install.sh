76ce5693d1352779e25339655328444af4b30eaf
Fredrik Frodlund <fredrik@frodlund.se>
2025-11-22 20:33:33 +0100

    feat(zoxide): add install script

---
+++ zoxide/install.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+#
+# Install zoxide on Linux systems
+
+case "$OSTYPE" in
+  linux*)
+    if ! command -v zoxide >/dev/null 2>&1 ; then
+
+      echo ""
+      echo "  ✅ Installing zoxide"
+      echo ""
+
+      curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
+
+    else
+
+      echo ""
+      echo " ⏭  zoxide is already installed! 👍"
+      echo ""
+
+    fi
+    ;;
+  *)
+    echo ""
+    echo " ⏭  Skipping zoxide installation (Linux only)"
+    echo ""
+    ;;
+esac
+
+exit 0
