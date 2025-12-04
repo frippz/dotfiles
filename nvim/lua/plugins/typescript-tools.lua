a3b541f3cca71eb538ebf27ddc820006cf6b9f1f
Fredrik Frodlund <fredrik@frodlund.se>
2025-12-04 07:50:54 +0100

    chore: whitespace

--- nvim/lua/plugins/typescript-tools.lua
+++ nvim/lua/plugins/typescript-tools.lua
@@ -1,12 +1,16 @@
 return {
   "pmizio/typescript-tools.nvim",
+
   enabled = false,
+
   dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+
   filetypes = {
     "typescript",
     "javascript",
     "svelte",
   },
+
   opts = {
     code_lens = "all",
   },
