30bac16f071a018c582b04fc4b394813ddc30ef4
Fredrik Frodlund <fredrik@frodlund.se>
2026-03-08 06:13:19 +0100

    feat(nvim): improve commenting in jsx/tsx/astro files

--- nvim/lua/plugins/comment_nvim.lua
+++ nvim/lua/plugins/comment_nvim.lua
@@ -2,4 +2,21 @@
 
 return {
   "numtostr/comment.nvim",
+
+  dependencies = {
+    "JoosepAlviste/nvim-ts-context-commentstring",
+  },
+
+  config = function()
+    require("ts_context_commentstring").setup({
+      enable_autocmd = false,
+      languages = {
+        astro = "{/* %s */}", -- use JS style comments in template
+      },
+    })
+
+    require("Comment").setup({
+      pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+    })
+  end,
 }
