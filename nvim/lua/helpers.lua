5abbf858212312179fd46b988e06a2b6e9a8a598
Fredrik Frodlund <fredrik@frodlund.se>
2026-04-13 07:02:56 +0200

    refactor(nvim): type map function

--- nvim/lua/helpers.lua
+++ nvim/lua/helpers.lua
@@ -4,6 +4,10 @@
 -- map function
 local mapOpts = { noremap = true, silent = true }
 
+---@param mode string|string[]
+---@param lhs string
+---@param rhs string|function
+---@param opts? vim.keymap.set.Opts
 function map(mode, lhs, rhs, opts)
   opts = vim.tbl_extend("force", mapOpts, opts or {})
   vim.keymap.set(mode, lhs, rhs, opts)
