e2222619b36ef13acf53f36c8be20bb906b18f63
Fredrik Frodlund <fredrik@frodlund.se>
2026-09-14 17:43:25 +0200

    conf(homebrew): split up formulas and casks

--- homebrew/aliases.zsh
+++ homebrew/aliases.zsh
@@ -1,7 +1,8 @@
 if command -v brew >/dev/null 2>&1; then
   alias brews='brew list --versions'
-  alias bubo='brew update-if-needed && brew outdated'
-  alias bubc='brew upgrade && brew cleanup'
+  alias bubo='brew update-if-needed && brew outdated --formula'
+  alias bubc='brew upgrade --formula && brew cleanup'
   alias bubu='bubo && bubc'
+  alias caskup='brew update-if-needed && brew outdated --cask && brew upgrade --cask && brew cleanup'
   alias bi='brew install'
 fi
